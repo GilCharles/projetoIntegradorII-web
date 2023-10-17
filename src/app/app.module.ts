6bc074935738747dd40ff173da60caf3a54dea1a
Gilvan Charles <gilvan.charles.silva@gmail.com>
2023-10-17 06:17:36 -0300

    pagina home está com botões criado, faltando apenas fazer um design melhor. Foi corrigido um problema de duplicidade no modulo style no angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from 'src/components/navbar/navbar.component';
+import { HomeActiveComponent } from 'src/components/home-page/page-active/home-active.component';
+import { RegisterCreditorComponent } from 'src/components/home-page/register-creditor/register-creditor.component';
+import { RegisterDebtorComponent } from 'src/components/home-page/register-debtor/register-debtor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeActiveComponent,
+    RegisterCreditorComponent,
+    RegisterDebtorComponent
   ],
   imports: [
     BrowserModule,
