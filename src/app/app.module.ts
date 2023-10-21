cb6d902397e1a9fa6faee0209267946820731c37
Gilvan Charles <gilvan.charles.silva@gmail.com>
2023-10-21 17:26:00 -0300

    Foram feitos os html base de registration e feito algumas pequenas modificações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from 'src/components/navbar/navbar.component';
-import { HomeActiveComponent } from 'src/components/home-page/page-active/home-active.component';
+import { HomeActiveComponent } from 'src/components/home-page/active/home-active.component';
 import { RegisterCreditorComponent } from 'src/components/home-page/register-creditor/register-creditor.component';
 import { RegisterDebtorComponent } from 'src/components/home-page/register-debtor/register-debtor.component';
+import { ActivatedComponent } from 'src/components/registration/activated/activated.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RegisterDebtorComponent } from 'src/components/home-page/register-debto
     NavbarComponent,
     HomeActiveComponent,
     RegisterCreditorComponent,
-    RegisterDebtorComponent
+    RegisterDebtorComponent,
+    ActivatedComponent
   ],
   imports: [
     BrowserModule,
